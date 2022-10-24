378482f1e3fcd1e8ce9a6f9660da46dcf53d48fd
Mark Farnsworth <farnsworth2008@gmail.com>
2022-10-24 17:46:29 +0000

    0.1.9 Updated `changes` module
    - Updated `changes` module

--- CHANGES.md
+++ CHANGES.md
@@ -1,8 +1,8 @@
 # Changes
 Recently completed and planned work is tracked here.
 
-## [0.0.1](.) to [0.1.7](.)
+## [0.0.1](.) to [0.1.8](.)
 - Created early versions
 
-## [0.1.8](.)
-- Added `terraform.tf`
+## [0.1.9](.)
+- Updated `changes` module
--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ locals {
 
 # This module is the primary user of the changes module.
 module "changes" {
-  source = "git::https://github.com/s3d-club/terraform-external-changes?ref=v0.1.10"
+  source = "git::https://github.com/s3d-club/terraform-external-changes?ref=v0.1.11"
 
   path = var.path
   tags = var.tags
